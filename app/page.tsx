6a955d04a7f2a062ce55c79247632cdf9ece356e
Davichobits <davidruizdeveloper@gmail.com>
2025-06-12 19:11:39 -0500

    Add About link to Home component for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center p-24">
       <span className="text-7xl">Home</span>
+      <Link href='/about'>About Page</Link>
     </div>
   );
 }
