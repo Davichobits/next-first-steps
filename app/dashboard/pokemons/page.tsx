7e2cdf1e45224b98ad0e68ada946aae5df0e92ea
Davichobits <davidruizdeveloper@gmail.com>
2025-06-20 16:48:41 -0500

    Refactor PokemonsPage to fetch and display Pokémon data from API

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -5,9 +5,19 @@ export const metadata: Metadata = {
   description: 'Manage your Pokemons',
 }
 
-const PokemonsPage = () => {
+const getPokemons = async(limit = 20, offset = 0) => {
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then(res => res.json());
+  
+  return data;
+}
+
+const PokemonsPage = async () => {
+  const pokemons = await getPokemons();
   return (
-    <div>PokemonsPage</div>
+    <div>
+      {JSON.stringify(pokemons)}
+    </div>
   )
 }
 
